ad97b6f480af65aad067f7b34bb231849eb33e3d
arvindzagade <arvindzagade@gmail.com>
2021-05-19 15:49:22 +0530

    Create 1.Reverse_array.py

---
+++ Array/1.Reverse_array.py
@@ -0,0 +1,17 @@
+### Using Function
+def reverse(arr):
+
+    rev =  arr[::-1]
+    print(rev)
+    ## Directly return op
+    # return arr[::-1]
+
+if __name__ == '__main__':
+    arr1 = [4, 5, 1, 2]
+    reverse(arr1)
+################# Using List Indexing in python ###
+"""arr1 = [4, 5, 1, 2]
+   arr = arr1[::-1]
+   print(arr)
+   
+"""
